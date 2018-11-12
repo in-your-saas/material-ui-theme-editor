b95f89a47c906abacb268f693cc5a96218b1518c
Jérémie Drouet <jeremie.drouet@gmail.com>
2018-11-12 08:37:20 +0100

    Updates

---
+++ precache-manifest.9d9be749ae1e4f1b7c24d68b24b624e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1e4b6563bf2d7b8671e",
+    "url": "/material-ui-theme-editor/static/js/runtime~main.a1e4b656.js"
+  },
+  {
+    "revision": "57cbd179742f9fc428d7",
+    "url": "/material-ui-theme-editor/static/js/main.57cbd179.chunk.js"
+  },
+  {
+    "revision": "d751410a92d5531aa521",
+    "url": "/material-ui-theme-editor/static/js/1.d751410a.chunk.js"
+  },
+  {
+    "revision": "57cbd179742f9fc428d7",
+    "url": "/material-ui-theme-editor/static/css/main.38224f44.chunk.css"
+  },
+  {
+    "revision": "585b54e960b8fd7d26d4973e5e39de8e",
+    "url": "/material-ui-theme-editor/index.html"
+  }
+];
